7729c989b3ac6cda0035c612c360e6ced1f8f3ba
Maharani Ladja <89758958+maharaniladja@users.noreply.github.com>
2021-10-31 23:22:18 +0800

    Add files via upload

---
+++ Chapter 6/Praktikum 2 No. 2 dan 6.py
@@ -0,0 +1,22 @@
+#PRAKTIKUM 2 NO.2
+def LuasSegitiga(a, t):
+    luas = a * t / 2
+    return luas
+
+alas = 10
+tinggi = 20
+print(' Luas Segitiga dengan alas ', alas,
+      ' dan tinggi', tinggi,
+      ' adalah ', LuasSegitiga(alas, tinggi))
+
+
+#PRAKTIKUM 2 NO.4
+def LuasSegitiga(a, t):
+    luas = a * t / 2
+    return luas
+
+alas = 15
+tinggi = 45
+print(' Luas Segitiga dengan alas ', alas,
+      ' dan tinggi', tinggi,
+      ' adalah ', LuasSegitiga(alas, tinggi))
---
+++ Chapter 6/Praktikum 2 No.3 dan 7.py
@@ -0,0 +1,21 @@
+#PRAKTIKUM 2 NO.3
+def LuasSegitiga2(a, t):
+    luas = a * t / 2
+    print('Luas Segitiga dengan alas ', alas,
+          ' dan tinggi ', tinggi,
+          ' adalah ', luas)
+
+alas = 10
+tinggi = 20 
+LuasSegitiga2(alas, tinggi)
+
+#PRAKTIKUM 2 NO.7
+def LuasSegitiga2(a, t):
+    luas = a * t / 2
+    print('Luas Segitiga dengan alas ', alas,
+          ' dan tinggi ', tinggi,
+          ' adalah ', luas)
+
+alas = 15
+tinggi = 45
+LuasSegitiga2(alas, tinggi)
